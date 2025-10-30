822ac4af167a6aaa05d7f7f616f42e44ea0d5868
Kristen Woerdeman <60305526+kwoerd@users.noreply.github.com>
2025-10-29 22:13:37 -0700

    Switch Termly to embed.min.js with website UUID to show in-modal Preferences button

--- components/termly-script.tsx
+++ components/termly-script.tsx
@@ -7,7 +7,9 @@ export default function TermlyScript() {
   return (
     <Script
       id="termly-consent-banner"
-      src="https://app.termly.io/resource-blocker/ba09ca99-2e6c-4e83-adca-6b3e27ffe054?autoBlock=on"
+      src="https://app.termly.io/embed.min.js"
+      data-auto-block="on"
+      data-website-uuid="ba09ca99-2e6c-4e83-adca-6b3e27ffe054"
       strategy="afterInteractive"
     />
   )
